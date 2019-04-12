8aae389849e47ea6b330e96d3a47e4364206f600
Bola Naguib <bolanaguib@gmail.com>
2019-04-12 04:17:21 +0200

    added

--- src/template/index.php
+++ src/template/index.php
@@ -1,20 +1,36 @@
 <!-- START header -->
 <?php get_header(); ?>
 
-<section class="section section_theme_midnight">
-  <div class="uk-container uk-container-large">
-    <div class="" uk-grid>
-      <div class="uk-width-1-2">
-        <h3> Title </h3>
-        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-      </div>
-      <div class="uk-width-1-2">
-
-      </div>
+<section class="uk-position-relative section_type_parallax">
+  <a href="#">
+  <div class="parallax uk-height-large uk-background-cover uk-overflow-hidden uk-light uk-flex uk-flex-top" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/media.jpg');">
+    <div class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">
+        <h1 class="parallax__title parallax__right" uk-parallax="opacity: 0,1; y: -100,0; x: -100,0; scale: 2,1; viewport: 0.1;">MEDIA</h1>
     </div>
-  </div>
-</section>
+</div>
+</a>
 
+</section>
+<section class="uk-position-relative section_type_parallax">
+  <a href="#">
+  <div class="parallax uk-height-large uk-background-cover uk-overflow-hidden uk-light uk-flex uk-flex-top" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/interior.jpg');">
+    <div class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">
+        <h1 class="parallax__title parallax__left" uk-parallax="opacity: 0,1; y: -100,0; x: 100,0; scale: 2,1; viewport: 0.1;">INTERIOR</h1>
+    </div>
+</div>
+</a>
+</section>
+<section class="uk-position-relative section_type_parallax">
+  <a href="#">
+
+  <div class="parallax uk-height-large uk-background-cover uk-overflow-hidden uk-light uk-flex uk-flex-top" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/cafe.jpg');">
+    <div class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">
+      <h1 class="parallax__title parallax__right" uk-parallax="opacity: 0,1; y: -100,0; x: -100,0; scale: 2,1; viewport: 0.1;">Coffe shop</h1>
+    </div>
+</div>
+</a>
+
+</section>
 
 
 
